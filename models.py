cb97e2caa36182c266ecb71ad83f575b164fdd4c
unknown <erikmoreirabarbato@gmail.com>
2025-04-22 12:06:42 -0300

    Criação das classes, do creat select

---
+++ models.py
@@ -0,0 +1,28 @@
+from peewee import *
+# Importar bibliotecas de data e hora para preencher automaticamente
+from datetime import datetime, date 
+
+meu_bd = SqliteDatabase("meus_dados.db")
+
+class MinhaBase(Model):
+    class Meta:
+        database = meu_bd
+
+class Usuario(MinhaBase):
+    nome = CharField()
+    email = CharField(unique=True)
+    senha = CharField
+
+class Tema(MinhaBase):
+    nome = CharField()
+
+class Arte(MinhaBase):
+    artista = ForeignKeyField(Usuario)
+    tipo = ForeignKeyField(Tema)
+    descricao = CharField()
+
+# Depois de criar todas as suas classes
+# Vamos criar o banco de dados e as tabelas
+meu_bd.connect()
+# meu_bd.create_tables([Estudante, TipoProtocolo, Protocolo])
+meu_bd.create_tables([Usuario, Tema, Arte])
